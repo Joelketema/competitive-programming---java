18521f11353de073b5ccb68649d65d6c2cef54e1
joelketema <eyuelket2019@gmail.com>
2022-12-26 21:36:23 +0300

    Started A2SV day 4

---
+++ src/ColorSort.java
@@ -0,0 +1,17 @@
+class ColorSort {
+    public void sortColors(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i] > nums[j]){
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+        }
+
+        for (int i : nums){
+            System.out.print(i);
+        }
+    }
+}
