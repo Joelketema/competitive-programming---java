82185a17ef159481f31f6a4cb1089870595d6373
joelketema <eyuelket2019@gmail.com>
2022-12-30 01:32:17 +0300

    Started A2SV day 5

---
+++ src/GoodPairs.java
@@ -0,0 +1,16 @@
+class GoodPairs {
+    public int numIdenticalPairs(int[] nums) {
+        int numberOfPairs = 0;
+
+        for(int i = 0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i] == nums[j]){
+                    numberOfPairs++;
+                }
+            }
+        }
+
+        return numberOfPairs;
+
+    }
+}
